325c95a9346bede73df5461f4131ae2839972328
Luiz Meirelles <lcoutojunior@gmail.com>
2023-06-19 20:31:29 -0300

    Setting env variables to DB.

--- src/db/Connection.js
+++ src/db/Connection.js
@@ -1,10 +1,13 @@
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
 
 export async function connectDB() {
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
-  //await mongoose.connect('mongodb://atm_machine:7H7ooEDuA7081AXi@sa-east-1.aws.data.mongodb-api.com/app/data-xhcff/endpoint/data/v1/test');
-  //mongodb+srv://atm_machine:<password>@atmmachine.xktogaf.mongodb.net/
-  await mongoose.connect('mongodb+srv://atmMachineUser:ZuKBk5rnq0Q2Q636@atmmachine.xktogaf.mongodb.net/atmMachineDB');
+  const user = process.env.DB_USER;
+  const pass = process.env.PASS;
+  const db = process.env.DB;
+  
+  await mongoose.connect(`mongodb+srv://${user}:${pass}@atmmachine.xktogaf.mongodb.net/${db}`);
   console.log("Mongo Dabase Connected.");
 }
 
