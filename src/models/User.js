e13aea7d8cc8ca5eaa96e14445a3474cfe15bb70
Luiz Meirelles <lcoutojunior@gmail.com>
2023-06-20 22:17:56 -0300

    Adding export default.

--- src/models/User.js
+++ src/models/User.js
@@ -13,7 +13,4 @@ const userSchema = new Schema({
 
 const User = mongoose.model("User", userSchema);
 
-export {
-  User,
-  userSchema
-}
+export default User;
--- src/services/User.js
+++ src/services/User.js
@@ -1,4 +1,4 @@
-import { User } from "../models/User.js";
+import User from "../models/User.js";
 
 export async function createUser(user) {
   const { name, birthdate, cpf } = user;
