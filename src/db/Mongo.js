f77a9dba5ab9b86696341412ec345054098dd3bf
Luiz Meirelles <lcoutojunior@gmail.com>
2023-06-21 04:34:17 -0300

    Renamed to Mongo.

--- src/db/Mongo.js
+++ src/db/Mongo.js
@@ -2,13 +2,13 @@ import mongoose from "mongoose";
 import dotenv from 'dotenv';
 dotenv.config();
 
-export async function connectDB() {
+export async function connectMongo() {
   const user = process.env.DB_USER;
   const pass = process.env.PASS;
   const db = process.env.DB;
   
   await mongoose.connect(`mongodb+srv://${user}:${pass}@atmmachine.xktogaf.mongodb.net/${db}`);
-  console.log("Mongo Dabase Connected.");
+  console.log("Mongo Database Connected.");
 }
 
 
