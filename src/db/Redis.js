ec7cc772fab4299866845e4eb24fb9494917520b
Luiz Meirelles <lcoutojunior@gmail.com>
2023-06-22 20:22:04 -0300

    Fixing missing URL container Redis Name.

--- src/db/Redis.js
+++ src/db/Redis.js
@@ -3,8 +3,9 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const redisUrl = process.env.URL_REDIS_CONTAINER;
 const redisPort = process.env.REDIS_PORT;
-const client = createClient({ url: `redis://127.0.0.1:${redisPort}` });
+const client = createClient({ url: `redis://${redisUrl}:${redisPort}` });
 
 export async function connectRedis() {
 
